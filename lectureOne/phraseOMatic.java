972875eec8bbae3f1648bea8ea6cc2e39c2e7f81
fontenet <fontenet@fontenets-MacBook-Pro.local>
2019-08-12 08:40:37 -0700

    add comments to phrase generator

--- lectureOne/phraseOMatic.java
+++ lectureOne/phraseOMatic.java
@@ -1,6 +1,6 @@
 public class phraseOMatic {
     public static void main (String[] args) {
-        //Phrases
+        //Phrases of arrays
         String [] wordListOne = {
             "live","from New York", "It's Saturday", "Night", "This is it", "Hear I stand"
         };
@@ -10,17 +10,20 @@ public class phraseOMatic {
         String [] wordListThree = {
             "live","from New York", "No hidden agenda", "We will", "May be your own", "You better remember"
         };
-        
+        // determing the number of words in the respective arrays
         int wordOneLength = wordListOne.length;
         int wordTwoLength = wordListTwo.length;
         int wordThreeLength = wordListThree.length;
 
+        // generating a random number from 0 to the length of each respective array
         int randomNumber1 = (int) (Math.random() * wordOneLength);
         int randomNumber2 = (int) (Math.random() * wordTwoLength);
         int randomNumber3 = (int) (Math.random() * wordThreeLength);
-
+        
+        // building a string phrase by picking a random word from each respective array
         String phrase = wordListOne[randomNumber1] + " " +  wordListTwo[randomNumber2] + " " +  wordListThree[randomNumber3];
-
+        
+        // printing phrase to the console
         System.out.println( "Here is the randomly generated phrase:" + " " + phrase);
     }
 }
